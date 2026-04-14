ed4312c1d76b47e247c180885e43d30826406338
Yurii <yurii@goxore.com>
2026-04-14 04:33:13 +0200

    fixed locale for berlin english

--- nixos/features/desktop.nix
+++ nixos/features/desktop.nix
@@ -36,18 +36,10 @@
       monospace = ["JetBrainsMono Nerd Font"];
     };
 
-    time.timeZone = "Europe/Kyiv";
+    time.timeZone = "Europe/Berlin";
     i18n.defaultLocale = "en_US.UTF-8";
     i18n.extraLocaleSettings = {
-      LC_ADDRESS = "uk_UA.UTF-8";
-      LC_IDENTIFICATION = "uk_UA.UTF-8";
-      LC_MEASUREMENT = "uk_UA.UTF-8";
-      LC_MONETARY = "uk_UA.UTF-8";
-      LC_NAME = "uk_UA.UTF-8";
-      LC_NUMERIC = "uk_UA.UTF-8";
-      LC_PAPER = "uk_UA.UTF-8";
-      LC_TELEPHONE = "uk_UA.UTF-8";
-      LC_TIME = "uk_UA.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
     };
 
     services.upower.enable = true;
