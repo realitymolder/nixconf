00cd911eb772fd4793dd2da11aa55a6cd2e5ba6e
Yurii <yurii@goxore.com>
2026-04-18 00:12:31 +0200

    added waterfox to config + default config

--- nixos/features/waterfox.nix
+++ nixos/features/waterfox.nix
@@ -1,8 +1,16 @@
-{self, ...}: {
+{self, lib, ...}: {
   flake.nixosModules.waterfox = {pkgs, ...}: {
     environment.systemPackages = [
       self.inputs.waterfox-flake.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
+
+    xdg.mime.defaultApplications = {
+      "application/x-extension-htm" = ["waterfox.desktop"];
+      "text/html" = ["waterfox.desktop"];
+      "x-scheme-handler/http" = ["waterfox.desktop"];
+      "x-scheme-handler/https" = ["waterfox.desktop"];
+      "x-scheme-handler/chrome" = ["waterfox.desktop"];
+    };
 
     persistance.data.directories = [
       ".waterfox"
