d55a6e5dd78e02475ae96daa9c156c8d5c9fc980
Daniel Toubul <daniel@toubul.eu>
2026-04-22 19:25:40 +0200

    added flutter to system

--- wrappedPrograms/flutter.nix
+++ wrappedPrograms/flutter.nix
@@ -3,6 +3,28 @@
     packages.flutter-dev = inputs.wrappers.lib.wrapPackage {
       inherit pkgs;
       package = pkgs.flutter;
+      runtimeInputs = [
+        pkgs.clang
+        pkgs.cmake
+        pkgs.ninja
+        pkgs.pkg-config
+        pkgs.gtk3
+        pkgs.at-spi2-atk
+        pkgs.libxkbcommon
+        pkgs.unzip
+        pkgs.xz
+        pkgs.zip
+        pkgs.android-tools
+        pkgs.cacert
+        pkgs.ungoogled-chromium
+        pkgs.jre
+      ];
+      env = {
+        ANDROID_HOME = "$HOME/Android/Sdk";
+        ANDROID_SDK_ROOT = "$HOME/Android/Sdk";
+        CHROME_EXECUTABLE = "${pkgs.ungoogled-chromium}/bin/chromium";
+        PATH = "${pkgs.android-tools}/bin:$HOME/Android/Sdk/cmdline-tools/latest/bin:$PATH";
+      };
     };
   };
 }
